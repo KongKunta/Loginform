c6eeec725d45b378fee30ffd9c9ac2888d76e2e2
KongKunta <kongkunta@yahoo.com>
2023-10-01 18:07:43 +0200

    added ahref for login and register as buttons in index

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const login = require('./routes/login');
 const app = express();
 dotenv.config()
 
-app.set('views', './views')
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
